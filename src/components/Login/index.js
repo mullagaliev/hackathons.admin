1056c207d6db5e4064fac74adfba31d93a9dcf4e
Andrew <andrew@mullagaliev.com>
2018-05-19 07:43:56 +0300

    Добавлена авторизация

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { connect } from "react-redux";
+import { loginByUsername } from '../../redux/actions/loginActions';
+import { setAuth } from '../../redux/actions/authActions';
+
+import {
+  Redirect
+} from 'react-router-dom';
 
 const LoginButton = styled.button`
   display: block;
@@ -22,7 +29,7 @@ const LoginForm = styled.form`
   }
 `;
 
-const Input  = styled.input`
+const Input = styled.input`
   min-height: 30px;
   border: 1px solid #DFDDDD;
   border-radius: 4px;
@@ -33,15 +40,48 @@ const Input  = styled.input`
 `;
 
 class Login extends Component {
+  handlerSubmit = (e) => {
+    e.preventDefault();
+    const { login, password } = this.state;
+    if (login !== '' && password !== '') {
+      this.props.loginByUsername(login, password);
+    }
+  };
+  state = {
+    login: '',
+    password: ''
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.success) {
+      this.props.setAuth(nextProps.token);
+    }
+  };
+
   render() {
+    const { login, password } = this.state;
+    if (this.props.success)
+      return <Redirect to='/dashboard'/>;
     return (
         <React.Fragment>
-          <LoginForm>
+          <LoginForm onSubmit={this.handlerSubmit}>
             <h2>Welcome!</h2>
             <label>Username</label>
-            <Input placeholder='User'/>
+            <Input placeholder='User' name='login' value={login}
+                   onChange={this.handleInputChange}/>
             <label>Password</label>
-            <Input placeholder='******' type='password'/>
+            <Input placeholder='******' type='password' value={password} name='password'
+                   onChange={this.handleInputChange}/>
             <LoginButton type='submit'>Login</LoginButton>
           </LoginForm>
         </React.Fragment>
@@ -52,4 +92,7 @@ class Login extends Component {
 Login.propTypes = {};
 Login.defaultProps = {};
 
-export default Login;
+// export default Login;
+export default connect((state) => {
+  return { success: state.login.success, token: state.login.data ? state.login.data.token : "" }
+}, { loginByUsername, setAuth })(Login);
